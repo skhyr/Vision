4d67fb89b141a913c345dff3c1bf18328a1b5add
Piotr Malatyński <piotrmalatynski1@gmail.com>
2022-08-27 14:04:13 +0200

    user endpoint

--- src/api/queries/user.rs
+++ src/api/queries/user.rs
@@ -1,16 +1,15 @@
 use crate::api::DbConn;
+use crate::repos::UserRepo;
 use crate::services::user as UserService;
-use crate::types::Info;
-use crate::utils::errors::Errors;
+use crate::types::{Info, User};
 use rocket::response::status;
 use rocket::serde::json::Json;
 use rocket::{get, routes, Route};
 use uuid::Uuid;
 
-#[get("/info/<id>")]
+#[get("/info/<id>", rank = 1)]
 async fn get_info(id: String, conn: DbConn) -> Result<Json<Info>, status::BadRequest<()>> {
     conn.run(move |c| {
-        println!("{:?}", id);
         let user_id = Uuid::parse_str(id.as_str())
             .map_err(|_| status::BadRequest(Some("Invalid Id")))
             .unwrap();
@@ -22,6 +21,20 @@ async fn get_info(id: String, conn: DbConn) -> Result<Json<Info>, status::BadReq
     .await
 }
 
+#[get("/<id>", rank = 2)]
+async fn get_user(id: String, conn: DbConn) -> Result<Json<User>, status::BadRequest<()>> {
+    conn.run(move |c| {
+        let user_id = Uuid::parse_str(id.as_str())
+            .map_err(|_| status::BadRequest(Some("Invalid Id")))
+            .unwrap();
+
+        UserRepo::get_by_id(user_id, c)
+            .map(|r| Json(r))
+            .map_err(|_| status::BadRequest(None))
+    })
+    .await
+}
+
 pub fn get_routes() -> Vec<Route> {
-    routes![get_info]
+    routes![get_info, get_user]
 }

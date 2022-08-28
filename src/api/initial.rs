ea805d401336eb2f6a7c87d285a7d8bce046bcc1
Piotr Malatyński <piotrmalatynski1@gmail.com>
2022-08-28 21:46:57 +0200

    CORS

--- src/api/initial.rs
+++ src/api/initial.rs
@@ -6,7 +6,9 @@ use rocket::figment::{
     util::map,
     value::{Map, Value},
 };
+use rocket::http::Method;
 use rocket::Rocket;
+use rocket_cors::{AllowedOrigins, CorsOptions};
 use rocket_sync_db_pools::{database, diesel};
 
 #[database("vision_db")]
@@ -24,11 +26,24 @@ pub fn init_routes() -> Rocket<rocket::Build> {
         "url" => std::env::var("DATABASE_URL").unwrap().into()
     };
 
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::all())
+        .allowed_methods(
+            vec![Method::Get, Method::Post, Method::Put]
+                .into_iter()
+                .map(From::from)
+                .collect(),
+        )
+        .allow_credentials(true)
+        .to_cors()
+        .unwrap();
+
     let config = Figment::from(Config::default())
         .merge(("port", port_number))
         .merge(("databases", map!["vision_db" => db]));
 
     rocket::custom(config)
         .attach(DbConn::fairing())
+        .attach(cors)
         .mount("/", queries::get_routes())
 }
